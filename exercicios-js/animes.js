7bf4c975c6cf547d3d39d73599f0c68432297d84
itsalle <paulaalemand@gmail.com>
2019-11-06 19:53:13 -0300

    primeiro commit

---
+++ exercicios-js/animes.js
@@ -0,0 +1,16 @@
+// WHILE
+
+// let a = 3;
+// let tentativas = 0;
+
+// while(tentativas <= 3) {
+// o que eu quero que seja executado enquanto a condição não é atendida
+// }
+
+let animes = ['Death Note', 'Boku No Hero', 'Dragon Ball', 'Cavaleiros do Zodíaco', 'Naruto', 'Super Onze', 'Nazo No Kanojo']; // lista
+let i = 0;
+
+while (i < animes.length){
+    console.log(animes[i]);
+    i++; // incremento
+}
---
+++ script.js
@@ -0,0 +1,20 @@
+// WHILE
+
+let a = 3;
+let tentativas = 0;
+
+while(tentativas <= 3) {
+o que eu quero que seja executado enquanto a condição não é atendida
+}
+
+let animes = ['Death Note', 'Boku No Hero', 'Dragon Ball', 'Cavaleiros do Zodíaco', 'Naruto', 'Super Onze', 'Nazo No Kanojo']; // lista
+let i = 0;
+
+
+
+
+// FOR
+
+// FOR IN
+
+// FOR OF
